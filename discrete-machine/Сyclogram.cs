5ab574adf8d98c33325bd8811b333364518805f7
Демьян <d.e.m.a@bk.ru>
2013-05-14 05:22:55 +0400

    sleeped.... update Cyclogram, no good

--- discrete-machine/Сyclogram.cs
+++ discrete-machine/Сyclogram.cs
@@ -7,18 +7,63 @@ using System.Threading.Tasks;
 
 namespace discrete_machine
 {
+    public class ConditionalOperator
+    {
+        private readonly Connector connector;
+        private readonly char @operator;
+        private readonly int operand;
+
+        public ConditionalOperator(Connector connector, char @operator, int operand)
+        {
+            this.connector = connector;
+            this.@operator = @operator;
+            this.operand = operand;
+        }
+
+        public bool Comparer()
+        {
+            switch(@operator)
+            {
+                case '<':
+                if(connector.Value < operand)
+                    return true;
+                    break;
+                case '>':
+                if(connector.Value > operand)
+                    return true;
+                    break;
+            }
+            return false;
+        }
+    }
+
     public class Сyclogram
     {
+        public int CurrentStep = 0;
         public IOperation[][] Operations { get; set; }
 
-        //public void Add()
-        //{ 
-        
-        //}
-
-        public void Step()
+        public void Add(ConditionalOperator @operator, int row)
         {
-            Operations[0][0].Execute();
+            if (@operator.Comparer()) CurrentStep = row;
+        }
+        
+        public void Add(IOperation operation, int row)
+        {
+            Operations[row].ToList().Add(operation);
+        }
+
+        public void Remove(IOperation operation, int row)
+        {
+            Operations[row].ToList().Remove(operation);
+        }
+
+        public void NextStep()
+        {
+            foreach (var operation in Operations[CurrentStep])
+            {
+                operation.Execute();
+            }
+            CurrentStep++;
         }
     }
 }
